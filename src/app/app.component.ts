28ada8d29f4a2ca3b3613f66239c6f294352724c
cornprinicess <xiaocorn96@gmail.com>
2019-10-11 23:20:45 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+const todos = [
+  // {
+  //   id: 1,
+  //   title: 'finish todo mvc',
+  //   done: false
+  // }
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: {
+    id: number,
+    title: string,
+    done: boolean
+  }[] = todos;
+
+  addTodo(e): void {
+    const titleText = e.target.value;
+    if (!titleText.length) {
+      return;
+    }
+
+    // const lastId = this.todos[this.todos.length - 1].id;
+    const last = this.todos[this.todos.length - 1];
+    // console.log(last); // undefined
+    this.todos.push({
+      id: last ? last.id + 1 : 1,
+      title: titleText,
+      done: false
+    });
+
+    // 清楚文本框
+    e.target.value = '';
+    console.log(e.keyCode);
+  }
+
+  print(): void {
+    console.log('The current todos: ', this.todos);
+  }
+}
